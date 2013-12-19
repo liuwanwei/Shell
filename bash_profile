d58a16428f923146597940401fb65311ac9aaedc
liuwanwei <liuwanwei@gmail.com>
2013-12-19 11:49:55 +0800

    second commit

---
+++ bash_profile
@@ -0,0 +1,24 @@
+alias ll='ls -l'
+
+# 设置登录用户的密码，用于后面的sudo命令。
+password=7
+
+# 切换测试环境和生产环境。
+alias local="echo $password | sudo -S cp /etc/hosts_local /etc/hosts"
+alias remote="echo $password | sudo -S cp /etc/hosts_remote /etc/hosts"
+
+# 重启阿帕奇服务器。
+alias arst="echo $password | sudo apachectl restart"
+
+# 登录曦光阿里云VPS
+alias sshx='ssh server@115.29.148.60' 
+
+# 登录linode VPS
+alias sshl='ssh root@173.255.253.207' 
+
+# 切换到老汤馆所在目录
+alias cdt='cd /Users/sungeo/www/tang/'
+
+# 查看当前目录的git状态。
+alias gst='git status'
+
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function error_handler()
+{
+    echo "安装出现错误，请修正后再试。"
+    exit
+}
+
+function last_words()
+{
+    echo "环境准备完毕，还有两件事情要做："
+    echo "1.修改 $hosts_local 和 $hosts_remote 的内容,分别对应本地环境和生产环境"
+    echo "2.修改 $target_profile , 将password变量修改为当前用户密码。"
+}
+
+origin_profile=./bash_profile
+target_profile=~/.bash_profile
+
+# 备份旧的个人配置文件。
+if [ -e $target_profile ];then
+    cp $target_profile $target_profile".bk"
+fi
+
+# 复制新的个人配置文件到目标。
+if  [ -e $origin_profile ];then
+    cp $origin_profile $target_profile
+fi
+
+
+
+# 创建测试环境、生产环境主机配置文件。
+hosts_origin=/etc/hosts
+hosts_remote=/etc/hosts_remote
+hosts_local=/etc/hosts_local
+
+if [ ! -e $hosts_remote ];then
+    cp $hosts_origin $hosts_remote
+    if [ $? != 0 ];then
+            error_handler
+    fi
+fi
+
+cp $hosts_origin $hosts_local
+if [ $? != 0 ];then
+    error_handler
+fi
+
+last_words
