eaa06bedf6eb7e1ef814a13e4e5a19f4147067c0
liuwanwei <liuwanwei@gmail.com>
2014-03-25 11:03:18 +0800

    将更新的目录显示到提示信息中。

--- upmall.sh
+++ upmall.sh
@@ -3,24 +3,29 @@
 # 自动更新代码，能更新阿里云服务器和本地开发环境两部分的代码。
 
 if [[ $1 = '-lxb' ]];then
-        echo "更新本机梁晓波API代码..."
-        cd ~/Coding/liangxiaobo/malladmin/
+        dir=~/Coding/liangxiaobo/malladmin/
+
+        cd $dir
         git pull
-        #echo "更新本机梁晓波微广场代码..."
+        #echo "更新本机梁晓波微广场代码
         #cd ~/Coding/liangxiaobo/mallservice/
         #git pull
 elif [[ $1 = '-my' ]];then
-        echo "更新本机iPhone代码..."
-        cd ~/Coding/maoyu417/mallios/
+        dir=~/Coding/maoyu417/mallios/
+        echo "更新本机iPhone代码 $dir"
+        cd $dir
         git pull
 else
         server='115.29.148.60'
         userAtServer='root'
-        echo "更新 $server 微广场和API代码..."
-        ssh $userAtServer@$server "cd /opt/webroot/mallservice;git pull;cd /opt/webroot/malladmin;git pull"
+        echo "更新 $server mallservice代码"
+        ssh $userAtServer@$server "cd /opt/webroot/mallservice;git pull"
+        echo "更新 $server malladmin代码"
+        ssh $userAtServer@$server "cd /opt/webroot/malladmin;git pull"
 
-        echo "更新本机API代码..."
-        cd ~/www/malladmin
+        dir=~/www/malladmin
+        echo "更新本机API代码 $dir" 
+        cd $dir
         git pull
 fi
 
