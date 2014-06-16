4b74341ee9a3344295c55f1de965bf65ccf93be6
liuwanwei <liuwanwei@gmail.com>
2014-06-16 14:53:52 +0800

    启动msgqueue之前，加入是否已启动的判断，避免启动两个msgqueue。

--- queue/start.sh
+++ queue/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-./msgqueue > /dev/null 2>&1 &
+count=`ps ax | grep msgqueue | grep -v grep | wc -l`
+if [ $count -gt 0 ];then
+	echo "msgqueue already exist!"
+else
+	./msgqueue > /dev/null 2>&1 &
+fi
 
 /alidata/server/php/bin/php ./SmsWorker.php >/dev/null 2>&1 &
 
