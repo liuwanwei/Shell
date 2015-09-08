fb492decbd8acdd1e0e0b02f2fc2735b27a0a6c5
liuwanwei <liuwanwei@gmail.com>
2015-09-08 17:55:45 +0800

    增加中文注释

--- smsqueue/DispatchPaper.php
+++ smsqueue/DispatchPaper.php
@@ -1,25 +1,24 @@
 <?php
 /*
- * Weather update server
- * Binds PUB socket to tcp://*:5556
- * Publishes random weather updates
- * @author Ian Barber <ian(dot)barber(at)gmail(dot)com>
+ * 发纸机发纸命令推送服务器，使用 ZeroMQ 的 Pub/Sub 模式
+ * 绑定 PUB socket to tcp://*:4446
+ *
+ * 发纸前，通过 REQ/RSP 方式从 Web Service 接收发纸命令
+ * 绑定 REP socket to tcp://*:4445
+ *
+ * 再将命令 PUB 到发纸机（也就是 Subscriber）
+ * 发纸机通过设置过滤，只接收跟自己别名相同的消息
+ *
+ * @author Liu Wanwei (liuwanwei@gmail.com)
  */
 
-// Prepare our context and publisher
-// Publish command to Android host machine
+// 跟发纸机通信的端口
 $context = new ZMQContext();
 $publisher = $context->getSocket(ZMQ::SOCKET_PUB);
 $publisher->bind("tcp://*:4446");
 $publisher->bind("ipc://papers.ipc");
 
-// receiver command from index.php?r=dispatch/publishToZMQ&alias=2xxxxxxx
-#$subscrIBER = NEW ZMQSocket($context, ZMQ::SOCKET_SUB);
-#$subscriber->connect("tcp://localhost:4445");
-#$subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, "dispatch");
-
-
-//  Socket to talk to clients
+// 跟 Web Service 通信的端口
 $responder = new ZMQSocket($context, ZMQ::SOCKET_REP);
 $responder->bind("tcp://*:4445");
 
